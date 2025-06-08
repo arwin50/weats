104f8ef1d141905fffb56f7ef8a2d3429366c794
POWCHU\Arwin Delasan <delasanarwin@gmail.com>
2025-06-08 11:37:16 +0800

    fix in routing

--- components/wizard/startingPage.tsx
+++ components/wizard/startingPage.tsx
@@ -10,7 +10,7 @@ import { useRouter } from "next/navigation";
 export default function StartingPage() {
   const router = useRouter();
   const handleBackToDashboard = () => {
-    router.push("@/app/dashboard");
+    router.push("/dashboard");
   };
   return (
     <div className="animate-gradient h-screen flex flex-col justify-center items-center bg-[#fdf1dc] text-cente px-4 overflow-hidden">
